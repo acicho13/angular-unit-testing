1d1e55c3795227154fbdb39d3216ba2859e78964
acicho13 <53127478+acicho13@users.noreply.github.com>
2021-09-10 17:57:48 -0700

    adding initial files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pluralsight's Unit Testing in Angular Course
+This course is up to date.
+
+To get started, clone the repo or download it
+
+npm install
+npm test
+
+
+## Notes
+# Types of Mocks
+Dummies - just objects that fill a place
+Stubs - object that has controllable behavior
+Spies - object that keeps track of methods and how many times and what params were called
+
---
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { HeroComponent } from "../hero/hero.component"
+
+describe('HeroComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeroComponent ],
+
+    })
+
+    fixture =  TestBed.createComponent(HeroComponent)
+  })
+
+  it('', () => {
+
+  })
+})
